8821ef904cdc05d66533d180db105d779468ee21
baguette <139591238+ilikebaguettes@users.noreply.github.com>
2026-03-09 05:13:37 -0700

    added wheat scuffed wheat field

--- Assets/Scripts/PlayerScaler.cs
+++ Assets/Scripts/PlayerScaler.cs
@@ -6,6 +6,12 @@ public class PlayerScaler : MonoBehaviour
     public float duration;
     public float targetScale;
 
+    //private BoxCollider boxCollider;
+
+    public Terrain terrain;
+    public float grassSlow;
+
+
     public void StartScaling()
     {
         StartCoroutine(ScaleOverTime(duration, targetScale));
@@ -13,12 +19,17 @@ public class PlayerScaler : MonoBehaviour
 
     private IEnumerator ScaleOverTime(float duration, float scale)
     {
+        //boxCollider = GetComponent<BoxCollider>();
         Vector3 startScale = transform.localScale;
         Vector3 endScale = Vector3.one * scale;
         float elapsed = 0f;
 
         while (elapsed < duration)
         {
+            terrain.terrainData.wavingGrassSpeed = grassSlow;
+
+            //boxCollider.size = new Vector3(endScale, endScale, endScale);
+
             float t = elapsed / duration;
             transform.localScale = Vector3.Lerp(startScale, endScale, t);
             elapsed += Time.deltaTime;
