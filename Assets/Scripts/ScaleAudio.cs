c32d4fb97b295bbcf91d88b1defb90877908f039
renoutoften <nguyenren64@gmail.com>
2026-03-30 02:23:49 -0700

    ambient sounds

---
+++ Assets/Scripts/ScaleAudio.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleAudio : MonoBehaviour
+{
+    public Transform player;
+    private List<AudioSource> audioSources;
+
+    public float minScale = 0.001f;
+    public float maxScale = 1f;
+
+    void Start()
+    {
+        audioSources = new List<AudioSource>(FindObjectsOfType<AudioSource>());
+    }
+
+    void Update()
+    {
+        float scale = player.localScale.x;
+        float normalized = Mathf.InverseLerp(minScale, maxScale, scale);
+        float pitch = Mathf.Lerp(0.2f, 1f, normalized);
+
+        foreach (AudioSource source in audioSources)
+        {
+            source.pitch = pitch;
+        }
+    }
+}
